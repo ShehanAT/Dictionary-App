ac423e5387e7c95e09c6391198a2a5dcc9937243
Nipun <shehanatuk@gmail.com>
2023-05-19 21:01:42 -0400

    Added more files to svelte-app/src folder

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,10 +1,44 @@
-<script>
+<script lang="ts">
 	export let name;
+    import { onMount } from "svelte";
+    import { supabase } from "$lib/db";
+    import Auth from "$lib/Auth.svelte";
+    import type { User } from "@supabase/supabase-js";
+    import Home from "$lib/Home.svelte";
+
+    let user: User;
+
+    onMount(() => {
+        supabase.auth.getSession().then(({ data: { session }}) => {
+            user = session?.user ?? null;
+        });
+
+        const { data: { subscription: authListener }} = supabase.auth.onAuthStateChange(
+            (_, session) => {
+                const currentUser = session?.user;
+                user = currentUser ?? null;
+            }
+        );
+
+        return () => {
+            authListener?.unsubscribe();
+        };
+    });
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+    <div class="w-full h-full bg-gray-200">
+        {#if user}
+          <Home bind:user />
+        {:else}
+          <div class="min-w-full min-h-screen flex items-center justify-center">
+            <Auth />
+          </div>
+        {/if}
+      </div>
 
 	<form class="form-wrapper cf">
 		<input type="text" placeholder="Search here..." required>
